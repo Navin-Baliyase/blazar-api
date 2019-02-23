bb75a28a982056bb4aaa8dec7814409bd10f2daa
Navin Kumar <navinkumar2508@gmail.com>
2019-02-23 14:04:00 +0530

    TECH:Navin | graph values | in range

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -3,6 +3,7 @@ class GraphsController < ApplicationController
 
 	def index
 
+=begin    
     #render json: Graph.all
     @graphs = Graph.all
     min = Graph.minimum(:PI)
@@ -19,37 +20,29 @@ class GraphsController < ApplicationController
         #average: average
         #value: @graph
     }.to_json
-
-
-=begin
-@graphs = Graph.all
-    min = Graph.minimum(:PI)
-    max = Graph.maximum(:PI)
-    name = Graph.pluck(:PI).sort
-    interval = (max-min+1)/@graphs.count
-    dup_min = min
-    dup_interval = interval
-    result_string = "(PI >= #{dup_min} AND PI <= #{dup_interval})"
-    ab = ["#{dup_min}..#{dup_min+dup_interval}"]
-    begin
-      result_string =+ result_string + " OR (PI >= #{dup_min} AND PI <= #{dup_interval})"
-      dup_min = dup_interval
-      # dup_interval = dup_interval + (dup_interval-min)
-      ab << "#{dup_min}..#{dup_interval}"
-    end while dup_interval < max
-    p result_string
-    # @graph = Graph.where(result_string).group(:name)
-    render json: {
-        x_axis: ab,
-        name: name
-        #min: min,
-        #max: max,
-        #interval: interval,
-        #value: @graph.pluck(:PI),
-    }.to_json
 =end
 
 
-  end
+    @graphs = Graph.all
+    min = Graph.minimum(:PI)
+    max = Graph.maximum(:PI)
+    name = Graph.pluck(:PI).sort
+    interval = (max-min+1)/10
+    dup_min = min
+    dup_interval = interval
+    result_string = "(PI >= #{dup_min} AND PI <= #{dup_interval})"
+    range = ["#{dup_min}-#{dup_interval}"]
+    begin
+      dup_min = dup_interval
+       dup_interval = dup_interval + (dup_interval-min)
+       result_string =+ result_string + " OR (PI >= #{dup_min} AND PI <= #{dup_interval})"
+        range << "#{dup_min}-#{dup_interval}"
+    end while dup_interval < max
+     @graph = Graph.where(result_string).group(:PI)
+    render json: {
+        X_axis: range,
+        Y_axis: @graph.count
+    }.to_json
 
+  end
 end
