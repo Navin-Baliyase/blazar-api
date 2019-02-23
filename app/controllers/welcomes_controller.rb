77d5d53896b8ebb4bf0869459c8eb107ade4adc3
Navin Kumar <navinkumar2508@gmail.com>
2019-02-23 12:12:32 +0530

    TECH:Navin | added home page | home page

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,4 +1,6 @@
 class WelcomesController < ApplicationController
+  skip_before_action :authenticate_user!
   def index
+  	render html: '<div><strong>Welcome to Blazar API,</strong> you need to login to perform any action here.</div>'.html_safe
   end
 end
